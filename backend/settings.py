9d76d393d7dba5b124628301316f64d099689839
johedvi <johan.linden14@gmail.com>
2024-11-22 16:56:33 +0100

    Switch DB

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ class Config:
     API_KEY = "RGAPI-ad0f9df6-5fb9-4c3d-aa86-0bdaf58b23b6"
     DEFAULT_REGION_CODE = 'eune1'
     DEFAULT_REGION = 'europe'    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///players.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BACKEND_URL = 'https://blackultrasflex-backend.onrender.com'
 
