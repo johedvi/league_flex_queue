0c7e22609bf335375adf6e844a7b6e8ad4ae2a23
johedvi <johan.linden14@gmail.com>
2024-11-22 11:08:27 +0100

    Update wsgi.py file

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,6 @@
 # backend/wsgi.py
 
-from app import socketio
+from app import app
 
-# Expose the SocketIO instance as the WSGI application
-application = socketio
-
-if __name__ == "__main__":
-    socketio.run(application)
+# Expose the Flask app as the WSGI application
+application = app
