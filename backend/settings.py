32b350d23c0792ffa366f0d5627e197b29795b06
johedvi <johan.linden14@gmail.com>
2024-11-22 17:27:55 +0100

    a

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ class Config:
     API_KEY = "RGAPI-ad0f9df6-5fb9-4c3d-aa86-0bdaf58b23b6"
     DEFAULT_REGION_CODE = 'eune1'
     DEFAULT_REGION = 'europe'    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql://blackultras_user:jQPWjQM6YsBgjyArqRGyhhAOgYWTSott@dpg-ct0ah3e8ii6s73fjvha0-a.frankfurt-postgres.render.com/blackultras"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BACKEND_URL = 'https://blackultrasflex-backend.onrender.com'
 
--- backend/wsgi.py
+++
@@ -1,6 +0,0 @@
-# backend/wsgi.py
-
-from app import app
-
-# Expose the Flask app as the WSGI application
-application = app
