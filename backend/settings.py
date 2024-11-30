af0e52e48c070a86e4b8c93763bf199bbe97b3c9
johedvi <johan.linden14@gmail.com>
2024-11-30 20:22:57 +0100

    Reset DB

--- backend/settings.py
+++ backend/settings.py
@@ -6,7 +6,7 @@ class Config:
     API_KEY = os.environ.get('API_KEY')
     DEFAULT_REGION_CODE = 'eune1'
     DEFAULT_REGION = 'europe'    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI =  'postgresql://blackultrasdb_user:W5NhDEgrkKjtdQJUPeFbGyXq8xXvmtg9@dpg-ct5mas3tq21c7399psi0-a.frankfurt-postgres.render.com/blackultrasdb'#os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BACKEND_URL = 'api.blackultras.com'
 
