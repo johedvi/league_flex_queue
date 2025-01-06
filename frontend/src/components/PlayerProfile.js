b3c57de650b0fad76cf40f88d01eed980a41e916
johedvi <johan.linden14@gmail.com>
2025-01-06 18:12:50 +0100

    Fix image size

--- frontend/src/components/PlayerProfile.js
+++ frontend/src/components/PlayerProfile.js
@@ -13,7 +13,7 @@ function PlayerProfile() {
     Jungle: '/jungle-removebg-preview.png',
     ADC: '/botlane-removebg-preview.png',
     Support: '/support-removebg-preview.png',
-    Undefined: '/question_mark.png',
+    Undefined: '/smaller_question_mark.png',
   };
 
   useEffect(() => {
