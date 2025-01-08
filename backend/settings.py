b02adc4c3eb022ce845330eda4101066bf5aff98
johedvi <johan.linden14@gmail.com>
2025-01-08 12:47:28 +0100

    Change region to correct

--- backend/settings.py
+++ backend/settings.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     API_KEY = os.environ.get('API_KEY')
-    DEFAULT_REGION_CODE = 'eune1'
+    DEFAULT_REGION_CODE = 'EUN1'
     DEFAULT_REGION = 'europe'    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
